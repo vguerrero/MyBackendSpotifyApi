5e7b4128ebca764c16f5afaf4b503acbbcc2e49e
vguerrero <vguerrero.net@gmail.com>
2025-09-12 00:11:28 -0500

    adding host in cors

--- src/main/java/com/codechallenge/spotify/codespotify/CorsConfig.java
+++ src/main/java/com/codechallenge/spotify/codespotify/CorsConfig.java
@@ -14,6 +14,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
                         .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("https://my-front-end-spotify-app.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
